735b62f985b8d5221cc9838fd4e6f61d1d4f51a3
Andrii Tugai <andrii.tugai@gmail.com>
2020-03-10 22:35:10 +0200

    2nd commit

--- lookup/views.py
+++ lookup/views.py
@@ -4,7 +4,10 @@ def home(request):
     import json
     import requests
 
-    api_request = requests.get("http://www.airnowapi.org/aq/observation/zipCode/current/?format=json&zipCode=89129&distance=5&API_KEY=C621724F-00B6-42C0-81C7-490E58F4E8FC")
+    api_request = requests.get(
+        "http://www.airnowapi.org/aq/observation/zipCode/current/?format=application/json&zipCode=89129&distance=5&API_KEY=C621724F-00B6-42C0-81C7-490E58F4E8FC"
+        )
+        # "http://www.airnowapi.org/aq/observation/zipCode/current/?format=application/json&zipCode=89129&distance=5&API_KEY=C621724F-00B6-42C0-81C7-490E58F4E8FC"
 
     try:
         content = api_request.content#.decode('UTF-8')
